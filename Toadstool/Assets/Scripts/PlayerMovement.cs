ccb42631f1b34e4cb519463402ea76d28b23bca4
conradcl <conradcl345@gmail.com>
2024-10-18 18:55:13 -0400

    Jump and gravity
    JUMP

--- Toadstool/Assets/Scripts/PlayerMovement.cs
+++ Toadstool/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Transactions;
@@ -11,17 +12,32 @@ public class PlayerMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 5f;
+    public float gravity = -20f;
+    public float jump = 5f;
 
     public float turnSmoothTime = 0.2f;
     public float directionSmoothTime = 0.2f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
 
     private float turnSmoothVelocity;
 
     private Vector3 currentDirection;
     private Vector3 directionVelocity;
+    private Vector3 velocity;
+
+    private bool isGrounded;
 
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -38,5 +54,13 @@ public class PlayerMovement : MonoBehaviour
 
             controller.Move(currentDirection.normalized * speed * Time.deltaTime);
         }
+        
+        if(Input.GetButtonDown("Jump")&& isGrounded){
+            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
